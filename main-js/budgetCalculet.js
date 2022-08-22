091857a642436e7321e06af1be6f529da2a1cd3c
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-08-22 14:10:47 +0600

    galary target all players name js

--- main-js/budgetCalculet.js
+++
@@ -1,1 +0,0 @@
-console.log("calculet js fill addd");
--- main-js/playersName.js
+++ main-js/playersName.js
@@ -1,1 +1,11 @@
-console.log("players js fill add");
+const playerCardbutton = document.querySelectorAll(
+  ".players-card-wreper .select-btn"
+);
+for (const items of playerCardbutton) {
+  items.addEventListener("click", function (event) {
+    const playersNameElement = document.querySelectorAll(".player-name");
+    const tarGetParentElement = event.target.parentElement;
+    const tarGetChildElement = tarGetParentElement.children[0].innerText;
+    console.log(tarGetChildElement);
+  });
+}
