93f8a8f740d1a6b37369830bbc81b51cebb96309
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-08-22 15:37:29 +0600

    select players name and display players name js

--- main-js/playersName.js
+++ main-js/playersName.js
@@ -1,11 +1,25 @@
 const playerCardbutton = document.querySelectorAll(
   ".players-card-wreper .select-btn"
 );
+const selectListParentElement = document.getElementById("select-players-name");
+let listGetLength = selectListParentElement.children.length;
+let listLength = listGetLength;
 for (const items of playerCardbutton) {
   items.addEventListener("click", function (event) {
-    const playersNameElement = document.querySelectorAll(".player-name");
     const tarGetParentElement = event.target.parentElement;
-    const tarGetChildElement = tarGetParentElement.children[0].innerText;
-    console.log(tarGetChildElement);
+    const tarGetPlayersName = tarGetParentElement.children[0].innerText;
+    const worning = document.getElementById("text-worning");
+    if (listLength < 5) {
+      selectListParentElement.innerHTML += `  <li class="font-normal my-5">
+                <span class="font-extrabold mr-5">${(listLength += 1)}.</span> ${tarGetPlayersName}
+              </li>`;
+      const selectButton = event.target;
+      selectButton.setAttribute("disabled", true);
+      console.log(selectButton);
+      worning.style.display = "none";
+    } else {
+      worning.style.display = "block";
+      selectButton.removeAttribute("disabled", false);
+    }
   });
 }
